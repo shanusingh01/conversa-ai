e42f7eab418aab1b3c77a6fd5a0824fa2347dcd6
Shanu Singh <shanusuryavanshi08@gmail.com>
2026-05-11 05:55:57 -1200

    Add ChatWindow UI

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,9 +1,33 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 function ChatWindow(){
     return(
-        <div>
-            ChatWindow
+        <div className="ChatWindow">
+
+            <div className="navbar">
+                <span>ConverseAI <i class="fa-solid fa-chevron-down"></i> </span>
+
+                <div className="userIconDiv">
+                    <span><i class="fa-solid fa-user"></i></span>
+                </div>
+
+            </div>
+
+            <Chat/>
+
+            <div className="chatInput">
+                
+                <div className="userInput">
+                    <input placeholder="Ask anything" />
+                    <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+                </div>
+
+                <p className="info">
+                    ConverseAI can make mistakes.Check important info See Cookie Prefrences.
+                </p>
+
+            </div>
         </div>
     );
 }
